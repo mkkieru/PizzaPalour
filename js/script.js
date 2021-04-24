a826ca5e2fcbceee3adcaa8f44adcb44cbab292e
mkkieru <mark.kieru@student.moringaschool.com>
2021-04-24 13:22:05 +0300

    add temporary changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function order() {
+function pizzaOrder() {
 
 
     function pizza(size, crust, topping) {
@@ -8,17 +8,92 @@ function order() {
     };
 
 
-    var Size = document.getElementById("size").value;
-    var Crust = document.getElementById("crust").value;
-    var Topping = document.getElementById("topping").value;
-    var deliver = document.getElementById("deliver").checked;
-    var pickUp = document.getElementById("pickUp").checked;
 
-    if (deliver === true) {
+
+    var newSize = document.getElementById("size").value;
+    var newCrust = document.getElementById("crust").value;
+    var newTopping = document.getElementById("topping").value;
+    var newDeliver = document.getElementById("deliver").checked;
+    var newPickUp = document.getElementById("pickUp").checked;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    if (newDeliver === true) {
         var location = prompt("Where would you like it delivered : ");
+    } else if (newPickUp === true) {
+        alert("Your order will be ready in 20 minutes.");
+    } else {
+        alert("Would you like it  delivered or you will pick it up");
+
     }
 
-    var result = new pizza(Size, Crust, Topping);
+
+    var result = new pizza(newSize, newCrust, newTopping);
 
     alert("Your order will be delivered to " + location);
 }
+
+var order = [];
+
+
+
+
+
+
+
+// code to print orders but prints out final value of num  total times 
+
+// function checkout() {
+//     var total = document.getElementById("number").value;
+
+//     for (var num = 0; num < total; num += 1) {
+
+//         $(document).ready(function() {
+//             $("ol").append("<li>" + num + "</li>");
+//         });
+//     }
+// }
